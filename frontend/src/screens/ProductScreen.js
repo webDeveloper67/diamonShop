afc9a31a0d8285be5718eff8523ff68fb8e4486f
mdee <design4web2014@gmail.com>
2021-02-03 15:27:04 +0100

    implementing react-router-dom

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from './../components/Rating';
+import products from './../products';
+
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id);
+
+  return (
+    <div>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush" />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default ProductScreen;
