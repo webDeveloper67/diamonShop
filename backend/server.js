819579c59e36d228e446077ab9da5becfb11aacf
mdee <design4web2014@gmail.com>
2021-02-07 16:12:52 +0100

    adding mongoose js to manage our database and colors package to backend to have more readable messages in terminal

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'; // commonjs
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -25,5 +27,6 @@ app.listen(
   PORT,
   console.log(
     `Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
