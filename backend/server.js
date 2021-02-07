0ed694386a7b4daf2fe6e44b80a0ffa7ffe14d5a
mdee <design4web2014@gmail.com>
2021-02-07 10:22:15 +0100

    initializing the db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express'); // commonjs
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express'; // commonjs
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 dotenv.config();
 
