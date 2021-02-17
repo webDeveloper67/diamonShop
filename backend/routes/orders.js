76d82f001cab159f6afc96ecfb4d86e556ff6a61
mdee <design4web2014@gmail.com>
2021-02-17 15:01:27 +0100

    geting order by its id

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
-import { addOrderItems } from '../controllers/orderCtrl.js';
+import { addOrderItems, getOrderById } from '../controllers/orderCtrl.js';
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
