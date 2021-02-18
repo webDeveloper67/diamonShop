c90de46d2dc88a50305f297570f72a4d56d43e48
mdee <design4web2014@gmail.com>
2021-02-18 16:16:42 +0100

    adding screen for order items on frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userRed';
-import { orderCreateReducer } from './reducers/ordersRed';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/ordersRed';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +23,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
